ca31d7d3243d57ab8c73e38ec62ee86ac5a8a07b
samclarke <sam@samclarke.com>
2012-11-23 01:34:25 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -r zip/Upload/*
+
 cp --parents inc/plugins/sceditor.php zip/Upload
 cp --parents inc/languages/english/admin/config_sceditor.lang.php zip/Upload
 cp --parents -r jscripts/sceditor/* zip/Upload
