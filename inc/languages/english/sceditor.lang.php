9d8314e5c2a3c8695eca5bd9f81aa7f4a9de79d7
samclarke <sam@samclarke.com>
2012-12-04 21:51:15 +0000

    Add language file for last commit. #1

---
+++ inc/languages/english/sceditor.lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$l['showeditor']                 = 'Which BBCode editor to show on post pages';
+$l['showeditor_sceditor']        = 'SCEditor';
+$l['showeditor_sceditor_source'] = 'SCEditor (source mode)';
+$l['showeditor_mycode']          = 'MyCode editor';
+$l['showeditor_none']            = 'No editor';
